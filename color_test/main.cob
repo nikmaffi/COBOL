4236cf218d92586c89d5a59c71c1193a43f58f41
Nik <nik02.maffi@gmail.com>
2026-03-27 11:59:19 +0100

    Added SCREEN SECTION color test.

---
+++ color_test/main.cob
@@ -0,0 +1,26 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBOL-COLOR-TEST.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 FG-COLOR PIC 9 VALUE IS ZERO.
+       77 BG-COLOR PIC 9 VALUE IS ZERO.
+       77 TEST-STR PIC X VALUE IS "@".
+       77 Y-VAL    PIC 9 VALUE IS 1.
+       77 X-VAL    PIC 9 VALUE IS 1.
+       PROCEDURE DIVISION.
+           PERFORM VARYING BG-COLOR FROM 0 BY 1 UNTIL BG-COLOR > 7
+               MOVE 1 TO Y-VAL
+
+               PERFORM VARYING FG-COLOR FROM 0 BY 1 UNTIL FG-COLOR > 7
+                   DISPLAY TEST-STR LINE Y-VAL COLUMN X-VAL
+                       BACKGROUND-COLOR BG-COLOR
+                       FOREGROUND-COLOR FG-COLOR
+                   ADD 1 TO Y-VAL
+               END-PERFORM
+               ADD 1 TO X-VAL
+
+               IF X-VAL IS GREATER THAN 41 THEN
+                   MOVE 1 TO X-VAL
+               END-IF
+           END-PERFORM
+           STOP RUN.
