9a3261434819cf4990084a1d413ab7e6e34326c3
Nik Maffi <nik02.maffi@gmail.com>
2026-04-02 12:13:08 +0200

    Added HELLO WORLD and changed Snake game directory name.

---
+++ hello_world/src/main.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    HELLO-WORLD.
+       AUTHOR.        Nicolo' Maffi.
+       DATE-WRITTEN.   3 jan 2021.
+       DATE-COMPILED.  3 jan 2021.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello World!"
+           STOP RUN.
+       END PROGRAM HELLO-WORLD.
