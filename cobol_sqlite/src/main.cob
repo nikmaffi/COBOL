9faba2f34ca5755cec1f584c7157bae2efb3d670
Nik Maffi <nik02.maffi@gmail.com>
2026-04-02 17:56:12 +0200

    Fixed COBOL sqlite3 test.

--- cobol_sqlite/src/main.cob
+++ cobol_sqlite/src/main.cob
@@ -1,20 +1,19 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. COBOL-SQLITE-TEST.
+       PROGRAM-ID. COBOL-SQLITE.
+       AUTHOR.     NIK MAFFI.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-           77 SQLITE           POINTER.
-           77 ERROR-CODE       PIC S9(005) COMP-5.
-           77 DATABASE-NAME    PIC X(015).
-           77 SQL-QUERY        PIC X(100).
-           77 CALLBACK-FUNC    USAGE PROCEDURE-POINTER.
-           77 COMMAND          PIC 9.
-           01 DATA-RECORD.
-               02 ID-FIELD     PIC 99.
-               02 NAME-FIELD   PIC X(30).
+       77 SQLITE           POINTER.
+       77 ERROR-CODE       PIC S9(5)  USAGE IS COMP-5.
+       77 DATABASE-NAME    PIC X(14)  VALUE IS "./data/data.db".
+       77 SQL-QUERY        PIC X(100).
+       77 CALLBACK-FUNC    USAGE PROCEDURE-POINTER.
+       77 COMMAND          PIC 9.
+       01 DATA-RECORD.
+           02 ID-FIELD     PIC 99.
+           02 NAME-FIELD   PIC X(30).
        PROCEDURE DIVISION.
            SET SQLITE TO NULL.
-
-           MOVE "DATA.DB" TO DATABASE-NAME
 
            PERFORM WITH TEST AFTER UNTIL COMMAND IS EQUAL TO 3
                MOVE LOW-VALUES TO SQL-QUERY
@@ -27,10 +26,13 @@
                DISPLAY "COMMAND> " WITH NO ADVANCING
                ACCEPT COMMAND
 
+               DISPLAY " "
+
                EVALUATE COMMAND
                    WHEN 1 PERFORM ADD-RECORD
                    WHEN 2 PERFORM LIST-DATA
                    WHEN 3 PERFORM EXIT-TEST
+                   WHEN OTHER DISPLAY "ERROR: NO OPTION " COMMAND "!"
                END-EVALUATE
            END-PERFORM
            STOP RUN.
@@ -91,7 +93,7 @@
            END-IF
 
            SET CALLBACK-FUNC TO ADDRESS
-               OF ENTRY "COBOL-SQLITE-TEST-CALLBACK"
+               OF ENTRY "COBOL-SQLITE-CALLBACK"
 
            MOVE "SELECT * FROM PEOPLE;" TO SQL-QUERY
 
@@ -109,4 +111,4 @@
            END-CALL.
 
        EXIT-TEST.
-       END PROGRAM COBOL-SQLITE-TEST.
+       END PROGRAM COBOL-SQLITE.
